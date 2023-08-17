390198cf7ad9a582fa4cb6df96547390b271b1e6
Bhanu <rmbswarnajith@gmail.com>
2023-08-17 21:00:20 +0530

    Connect MongoDB

---
+++ src/main/java/com/project/movies/MoviesController.java
@@ -0,0 +1,25 @@
+package com.project.movies;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/movies")
+public class MoviesController {
+
+    @Autowired
+    private MoviesService moviesService;
+
+    @GetMapping
+    public ResponseEntity<List<Movies>> getAllMovies(){
+      return new ResponseEntity<List<Movies>>(moviesService.allMovies(),HttpStatus.OK);
+    }
+
+}
